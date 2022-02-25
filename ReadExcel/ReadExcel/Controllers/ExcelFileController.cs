264c87c277ed1fec2b7ea25a4410cdcc8cb0b467
Hiruni Nawanjana <nawanjuhiru2000@gmail.com>
2022-02-25 13:00:04 +0530

    first commit

---
+++ ReadExcel/ReadExcel/Controllers/ExcelFileController.cs
@@ -0,0 +1,76 @@
+﻿using ReadExcel.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace ReadExcel.Controllers
+{
+    public class ExcelFileController : Controller
+    {
+        // GET: ExcelFile
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Import(HttpPostedFileBase excelFile)
+        {
+            if (excelFile.ContentLength == 0 || excelFile == null)
+            {
+                ViewBag.Error = "Please Select Excel File";
+                return View("Index");
+            }
+            else
+            {
+                if (excelFile.FileName.EndsWith("xlsm"))
+                {
+                    Excel.Application application = new Excel.Application();
+                    Excel.Workbook workbook = application.Workbooks.Open("C:/Users/User/Downloads/" + excelFile.FileName);
+                    Excel.Worksheet worksheet = workbook.ActiveSheet;
+                    Excel.Range range = worksheet.UsedRange;
+
+                    List<ExcelPackageExtensions> excel = new List<ExcelPackageExtensions>();
+
+                    for(int row=2; row <=range.Rows.Count; row++)
+                    {
+                        ExcelPackageExtensions exc = new ExcelPackageExtensions();
+                        exc.SiteId = ((Excel.Range)range.Cells[row, 1]).Text;
+                        exc.Sector = ((Excel.Range)range.Cells[row, 2]).Text;
+                        exc.UpgradeBatch = ((Excel.Range)range.Cells[row, 3]).Text;
+                        exc.UpgradeType = ((Excel.Range)range.Cells[row, 4]).Text;
+                        exc.PriorityArea = ((Excel.Range)range.Cells[row, 5]).Text;
+                        exc.PlanMonth = ((Excel.Range)range.Cells[row, 6]).Text;
+                        exc.PlanWeek = ((Excel.Range)range.Cells[row, 7]).Text;
+                        exc.DoneDate = ((Excel.Range)range.Cells[row, 8]).Text;
+                        exc.Status = ((Excel.Range)range.Cells[row, 9]).Text;
+                        exc.Reason = ((Excel.Range)range.Cells[row, 10]).Text;
+                        exc.Remarks = ((Excel.Range)range.Cells[row, 11]).Text;
+                        exc.QOS_Status = ((Excel.Range)range.Cells[row, 12]).Text;
+
+                        excel.Add(exc);
+                    }
+
+                    if (excel != null)
+                    {
+                        ViewBag.ExcelList = excel;
+                        return View("View");
+                    }
+                    else
+                    {
+                        ViewBag.Error = "Something went wrong";
+                        return View("Index");
+                    }
+                }
+                else
+                {
+                    ViewBag.Error = "Something went wrong";
+                    return View("Index");
+                }
+            }
+        }
+    }
+}
---
+++ ReadExcel/ReadExcel/Models/ExcelPackageExtensions.cs
@@ -0,0 +1,25 @@
+﻿using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace ReadExcel.Models
+{
+    public class ExcelPackageExtensions
+    {
+        public string SiteId { get; set; }
+        public string Sector { get; set; }
+        public string UpgradeBatch { get; set; }
+        public string UpgradeType { get; set; }
+        public string PriorityArea { get; set; }
+        public string PlanMonth { get; set; }
+        public string PlanWeek { get; set; }
+        public string DoneDate { get; set; }
+        public string Status { get; set; }
+        public string Reason { get; set; }
+        public string Remarks { get; set; }
+        public string QOS_Status { get; set; }
+    }
+}
